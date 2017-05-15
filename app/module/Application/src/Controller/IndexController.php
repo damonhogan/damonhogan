768c098115a6e1e9288733c9fb10ea3a3fc12f80
Damon Hogan <damonhogan2@icloud.com>
2017-05-15 15:56:57 -0600

    populate signup data

--- app/module/Application/src/Controller/IndexController.php
+++ app/module/Application/src/Controller/IndexController.php
@@ -14,13 +14,55 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+
+         $data = [
+            "SourceEmail" => "test@propay.com",
+            "FirstName" => "John",
+            "MiddleInitial"=> "Q",
+            "LastName"=> "Test",
+            "BusinessLegalName"=> "ProPay Partner",
+            "DoingBusinessAs"=> "PPA",
+            "DateOfBirth"=> "1/19/1997",
+            "DayPhone"=> "8601233421",
+            "EveningPhone"=> "8601233421",
+            "SocialSecurityNumber"=> "111111111",
+            "ExternalId"=> "2212157",
+            "Tier"=> "Merchant",
+            "PhonePIN"=> "1234",
+            "Address"=> [
+                "ApartmentNumber"=> null,
+                "Address1"=> "100 Main Street",
+                "Address2"=> null,
+                "City"=> "Rocky Hill",
+                "State"=> "CT",
+                "Country"=> "USA",
+                "Zip"=> "06067"
+            ],
+            "BankAccount" => [
+                "AccountCountryCode"=>"USA",
+                "AccountOwnershipType"=>"Personal",
+                "AccountType"=>"C",
+                "BankAccountNumber"=>"123456789",
+                "BankName"=>"Wells Fargo",
+                "RoutingNumber"=>"102000076"
+            ],
+            "BusinessAddress"=> [
+                "Address1"=> "101 Main Street",
+                "Address2"=> "Ste. 200",
+                "City"=> "Rocky Hill",
+                "State"=> "CT",
+                "Country"=> "USA",
+                "Zip"=> "06067"
+            ]
+        ];
+
         $authCode = 'TXlUZXJtSWQ6TXlDZXJ0U3Ry';
         //$data = array("name" => "Hagrid", "age" => "36");
-        $data = [];
+        //$data = [];
         $data_string = json_encode($data);
 
-        $ch = curl_init('https://xmltestapi.propay.com/ProPayAPI');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        $ch = curl_init('https://xmltestapi.propay.com/ProPayAPI/signup');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -31,5 +73,9 @@ class IndexController extends AbstractActionController
 
         $result = curl_exec($ch);
         return new ViewModel(['result'=>$result]);
+
+
+        //current response
+        //'{"AccountNumber":0,"Password":null,"SourceEmail":null,"Status":"59","Tier":null}'
     }
 }
