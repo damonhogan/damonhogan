0d609e7ff5cf21028408107824e63a92115c8473
Damon Hogan <damonhogan2@icloud.com>
2017-05-15 15:06:01 -0600

    start of rest testing, setting up curl post with Auth token

--- app/module/Application/src/Controller/IndexController.php
+++ app/module/Application/src/Controller/IndexController.php
@@ -14,6 +14,22 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $authCode = 'TXlUZXJtSWQ6TXlDZXJ0U3Ry';
+        //$data = array("name" => "Hagrid", "age" => "36");
+        $data = [];
+        $data_string = json_encode($data);
+
+        $ch = curl_init('https://xmltestapi.propay.com/ProPayAPI');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string),
+                'Authorization: ' . 'Basic ' . $authCode)
+        );
+
+        $result = curl_exec($ch);
+        return new ViewModel(['result'=>$result]);
     }
 }
