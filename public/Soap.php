9430c2e1dbe79fa879f95bacb0a0f15ee9126050
Damon Hogan <damonhogan2@icloud.com>
2017-05-15 12:32:59 -0600

    zf3 setup

--- public/Soap.php
+++ public/Soap.php
@@ -1,7 +1,30 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: pi
+ * User: Damon Hogan
  * Date: 05/05/17
  * Time: 17:56
  */
+
+abstract class SOAPable {
+    public function getAsSOAP() {
+        foreach($this as $key=>&$value) {
+            $this->prepareSOAPrecursive($this->$key);
+        }
+        return $this;
+    }
+
+    private function prepareSOAPrecursive(&$element) {
+        if(is_array($element)) {
+            foreach($element as $key=>&$val) {
+                $this->prepareSOAPrecursive($val);
+            }
+            $element=new SoapVar($element,SOAP_ENC_ARRAY);
+        }elseif(is_object($element)) {
+            if($element instanceof SOAPable) {
+                $element->getAsSOAP();
+            }
+            $element=new SoapVar($element,SOAP_ENC_OBJECT);
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,75 @@
 <?php
-    phpinfo();
+    require_once('Soap.php');
+    //phpinfo();
+// ------------------------------------------
+//  ABSTRACT EXAMPLE
+// ------------------------------------------
+
+class PersonList extends SOAPable {
+    protected $ArrayOfPerson; // variable MUST be protected or public!
+}
+
+class Person extends SOAPable {
+    //any data
+}
+
+//$client=new SoapClient("test.wsdl", array( 'soap_version'=>SOAP_1_2, 'trace'=>1, 'classmap' => array('Person' => "Person", 'PersonList' => "PersonList")  ));
+
+//$PersonList=new PersonList;
+
+// some actions
+
+//$PersonList->getAsSOAP();
+
+//$client->someMethod($PersonList);
+$soapClient = new SoapClient("https://soapserver.example.com/blahblah.asmx?wsdl");
+
+// Prepare SoapHeader parameters
+$sh_param = array(
+    'Username'    =>    'username',
+    'Password'    =>    'password');
+$headers = new SoapHeader('http://soapserver.example.com/webservices', 'UserCredentials', $sh_param);
+
+// Prepare Soap Client
+$soapClient->__setSoapHeaders(array($headers));
+
+// Setup the RemoteFunction parameters
+$ap_param = array(
+    'amount'     =>    $irow['total_price']);
+
+// Call RemoteFunction ()
+$error = 0;
+try {
+    $info = $soapClient->__call("RemoteFunction", array($ap_param));
+} catch (SoapFault $fault) {
+    $error = 1;
+    print(" 
+            alert('Sorry, blah returned the following ERROR: ".$fault->faultcode."-".$fault->faultstring.". We will now take you back to our home page.'); 
+            window.location = 'main.php'; 
+            ");
+}
+
+if ($error == 0) {
+    $auth_num = $info->RemoteFunctionResult;
+
+    if ($auth_num < 0) {
+        //....
+
+        // Setup the OtherRemoteFunction() parameters
+        $at_param = array(
+            'amount'        => $irow['total_price'],
+            'description'    => $description);
+
+        // Call OtherRemoteFunction()
+        $trans = $soapClient->__call("OtherRemoteFunction", array($at_param));
+        $trans_result = $trans->OtherRemoteFunctionResult;
+        //....
+    } else {
+        // Record the transaction error in the database
+
+        // Kill the link to Soap
+        unset($soapClient);
+    }
+}
+
 ?>
