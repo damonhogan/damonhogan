f360d5aefed4722cfffb0197eda4af30bdba992e
Damon Hogan <damonhogan2@icloud.com>
2017-05-16 22:14:54 -0600

    work in progress

--- app/module/Application/src/Controller/IndexController.php
+++ app/module/Application/src/Controller/IndexController.php
@@ -12,11 +12,19 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+	
+	
     public function indexAction()
     {
-
+		 /**
+		  * This would mormally be in a config or in your dataase
+		  */  
+         private $_certString = '';
+         
+         
+         // regular merchant
          $data = [
-            "SourceEmail" => "test@propay.com",
+            "SourceEmail" => "damonhogan2@icloud.com",
             "FirstName" => "John",
             "MiddleInitial"=> "Q",
             "LastName"=> "Test",
@@ -27,7 +35,7 @@ class IndexController extends AbstractActionController
             "EveningPhone"=> "8601233421",
             "SocialSecurityNumber"=> "111111111",
             "ExternalId"=> "2212157",
-            "Tier"=> "Merchant",
+            "Tier"=> "", // "Merchant"
             "PhonePIN"=> "1234",
             "Address"=> [
                 "ApartmentNumber"=> null,
@@ -55,27 +63,32 @@ class IndexController extends AbstractActionController
                 "Zip"=> "06067"
             ]
         ];
+        
+        //TODO: business merchant. EIN....
+
+       
 
         $authCode = 'TXlUZXJtSWQ6TXlDZXJ0U3Ry';
-        //$data = array("name" => "Hagrid", "age" => "36");
-        //$data = [];
         $data_string = json_encode($data);
 
         $ch = curl_init('https://xmltestapi.propay.com/ProPayAPI/signup');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getCert()....  'TiAuNrNwEjRnScCaE9RcTcS7ReI9NG:ReI9NG');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string),
-                'Authorization: ' . 'Basic ' . $authCode)
-        );
+                'Content-Length: ' . strlen($data_string)
+        ));
 
         $result = curl_exec($ch);
         return new ViewModel(['result'=>$result]);
 
 
         //current response
-        //'{"AccountNumber":0,"Password":null,"SourceEmail":null,"Status":"59","Tier":null}'
+        //'{"AccountNumber":32291150,"Password":"!#GD$ADXv2","SourceEmail":"damonhogan2@icloud.com","Status":"00","Tier":"Platinum"}'
     }
-}
+    
+    public function getCert() {
+	  return $this->_certString;	
+    }
